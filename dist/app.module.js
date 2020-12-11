de976a36b17ef5d053a472e5db931ef1231dd8db
이기림(Kirim Lee)/Biz플랫폼개발팀/SKP <kirim@sk.com>
2020-12-11 19:53:30 +0900

    add package and remove all

--- dist/app.module.js
+++ dist/app.module.js
@@ -8,15 +8,13 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.AppModule = void 0;
 const common_1 = require("@nestjs/common");
-const app_controller_1 = require("./app.controller");
-const app_service_1 = require("./app.service");
 let AppModule = class AppModule {
 };
 AppModule = __decorate([
     common_1.Module({
         imports: [],
-        controllers: [app_controller_1.AppController],
-        providers: [app_service_1.AppService],
+        controllers: [],
+        providers: [],
     })
 ], AppModule);
 exports.AppModule = AppModule;
