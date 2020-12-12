9b60b04c1b2011635a46f97d6a8286f8f06c9212
이기림(Kirim Lee)/Biz플랫폼개발팀/SKP <kirim@sk.com>
2020-12-12 12:22:02 +0900

    add dto

--- src/restaurant/restaurant.resolver.ts
+++ src/restaurant/restaurant.resolver.ts
@@ -1,10 +1,16 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { Restaurant } from './entities/restaurant.entity';
+import { CreateRestaurantDto } from './dto/create-restaurant.dto';
 
 @Resolver(of => Restaurant)
 export class RestaurantResolver {
-  @Query(returns => Restaurant)
-  myRestaurant() {
-    return { name: 'suji' };
+  @Query(returns => [Restaurant])
+  restaurant(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
+    return [];
+  }
+
+  @Mutation(returns => Boolean)
+  createRestaurant(@Args() createRestaurantArgs: CreateRestaurantDto) {
+    return true;
   }
 }
