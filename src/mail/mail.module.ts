7971cc8caea7cb9bbe67572b9948fff5ecd60f63
Krim Lee <uthi1004@gmail.com>
2020-12-20 15:33:26 +0900

    mail 서비스 구현

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,14 +1,16 @@
-import { DynamicModule, Module } from '@nestjs/common';
+import { DynamicModule, Global, Module } from '@nestjs/common';
 import { CONFIG_OPTIONS } from 'src/common/common.constant';
 import { MainModuleOption } from './mail.interface';
+import { MailService } from './mail.service';
 
 @Module({})
+@Global()
 export class MailModule {
   static forRoot(options: MainModuleOption): DynamicModule {
     return {
       module: MailModule,
-      providers: [{ provide: CONFIG_OPTIONS, useValue: options }],
-      exports: [],
+      providers: [{ provide: CONFIG_OPTIONS, useValue: options }, MailService],
+      exports: [MailService],
     };
   }
 }
---
+++ src/mail/mail.service.ts
@@ -0,0 +1,35 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { CONFIG_OPTIONS } from 'src/common/common.constant';
+import { MainModuleOption } from './mail.interface';
+import got from 'got';
+import * as FormData from 'form-data';
+
+@Injectable()
+export class MailService {
+  constructor(
+    @Inject(CONFIG_OPTIONS) private readonly options: MainModuleOption,
+  ) {}
+
+  private async sendEmail(subject: string, content: string, to: string) {
+    const form = new FormData();
+    form.append('from', `nuber@${this.options.domain}`);
+    form.append('to', to);
+    form.append('subject', subject);
+    form.append('text', content);
+
+    const res = await got(
+      `https://api.mailgun.net/v3/${this.options.domain}/messages`,
+      {
+        headers: {
+          Authorization: `Basic ${Buffer.from(
+            `api:${this.options.apiKey}`,
+          ).toString('base64')}`,
+        },
+        method: 'POST',
+        body: form,
+      },
+    );
+
+    console.log(res.body);
+  }
+}
