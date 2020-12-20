ffb5b6b8482dae742175961b2a06de3a63012bc4
Krim Lee <uthi1004@gmail.com>
2020-12-20 16:00:08 +0900

    mail 서비스 템플릿으로 변경

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -10,12 +10,14 @@ export class MailService {
     @Inject(CONFIG_OPTIONS) private readonly options: MainModuleOption,
   ) {}
 
-  private async sendEmail(subject: string, content: string, to: string) {
+  private async sendEmail(subject: string, to: string) {
     const form = new FormData();
     form.append('from', `nuber@${this.options.domain}`);
     form.append('to', to);
     form.append('subject', subject);
-    form.append('text', content);
+    form.append('template', 'verify-email');
+    form.append('v:username', 'aaa');
+    form.append('v:code', 'code name');
 
     const res = await got(
       `https://api.mailgun.net/v3/${this.options.domain}/messages`,
