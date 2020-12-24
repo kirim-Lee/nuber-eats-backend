65176f3b3f76372d26a9c76b1dd8d3c3de51e69c
Kirim Lee <uthi1004@gmail.com>
2020-12-24 23:17:10 +0900

    test: mail service send email

--- src/mail/mail.service.spec.ts
+++ src/mail/mail.service.spec.ts
@@ -1,11 +1,13 @@
 import { Test } from '@nestjs/testing';
 import { CONFIG_OPTIONS } from 'src/common/common.constant';
 import { MailService } from './mail.service';
+import * as FormData from 'form-data';
+import got from 'got';
 
-jest.mock('got', jest.fn());
-jest.mock('form-data', () => ({
-  append: jest.fn(),
-}));
+jest.mock('got');
+jest.mock('form-data');
+
+const TEST_DOMAIN = 'test-domain';
 
 describe('MailService', () => {
   let service: MailService;
@@ -17,7 +19,7 @@ describe('MailService', () => {
           provide: CONFIG_OPTIONS,
           useValue: {
             apiKey: 'test-apiKey',
-            domain: 'test-domain',
+            domain: TEST_DOMAIN,
             fromEmail: 'test-email',
           },
         },
@@ -30,7 +32,37 @@ describe('MailService', () => {
     expect(service).toBeDefined();
   });
 
-  it.todo('sendEmail');
+  describe('sendEmail', () => {
+    it('should send email', async () => {
+      const sendEmail = Reflect.get(service, 'sendEmail');
+      await sendEmail.call(service, 'test', 'ee', 'eee', [
+        { key: '1', value: '1' },
+        { key: '2', value: '2' },
+      ]);
+
+      const formAppend = jest.spyOn(FormData.prototype, 'append');
+
+      expect(formAppend).toHaveBeenCalledTimes(6);
+      expect(got).toHaveBeenCalledTimes(1);
+      expect(got).toHaveBeenCalledWith(
+        `https://api.mailgun.net/v3/${TEST_DOMAIN}/messages`,
+        expect.any(Object),
+      );
+    });
+
+    it('should return error when send email failed', async () => {
+      const sendEmail = Reflect.get(service, 'sendEmail');
+      (got as any).mockImplementation(() => {
+        throw Error('got implementation error');
+      });
+      const consoleLog = jest.spyOn(console, 'log');
+      await sendEmail.call(service, 'test', 'ee', 'eee', []);
+      expect(consoleLog).toHaveBeenCalledTimes(1);
+      expect(consoleLog).toHaveBeenCalledWith(
+        Error('got implementation error'),
+      );
+    });
+  });
 
   describe('sendVerificationEmail', () => {
     it('should call sendEmail', async () => {
