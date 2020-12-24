b76bcd59a073dc28e8864ed0904716dbcdcfe3a8
Kirim Lee <uthi1004@gmail.com>
2020-12-24 21:42:23 +0900

    test: email service verification email test

--- src/mail/mail.service.spec.ts
+++ src/mail/mail.service.spec.ts
@@ -29,4 +29,30 @@ describe('MailService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it.todo('sendEmail');
+
+  describe('sendVerificationEmail', () => {
+    it('should call sendEmail', async () => {
+      const SEND_EMAIL = 'test@email.com';
+      const SEND_CODE = 'code123';
+
+      const sendEmail = jest
+        .spyOn(service as any, 'sendEmail')
+        .mockImplementation(jest.fn());
+
+      service.sendVerificationEmail(SEND_EMAIL, SEND_CODE);
+      expect(sendEmail).toHaveBeenCalledTimes(1);
+
+      expect(sendEmail).toHaveBeenCalledWith(
+        SEND_EMAIL,
+        expect.any(String),
+        expect.any(String),
+        [
+          { key: 'code', value: SEND_CODE },
+          { key: 'username', value: SEND_EMAIL },
+        ],
+      );
+    });
+  });
 });
