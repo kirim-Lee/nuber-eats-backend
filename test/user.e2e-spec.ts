fa53ea74b238fe1b4860a2c93a69629b42cca541
Kirim Lee <uthi1004@gmail.com>
2020-12-26 16:05:09 +0900

    test: e2e user profile

--- test/user.e2e-spec.ts
+++ test/user.e2e-spec.ts
@@ -1,8 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import { AppModule } from '../src/app.module';
-import { getConnection } from 'typeorm';
+import { getConnection, Repository } from 'typeorm';
 import * as request from 'supertest';
+import { User } from 'src/users/entities/user.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
 jest.mock('got', () => {
   return {
@@ -16,6 +18,7 @@ const PASSWORD = '12345';
 
 describe('UserModule (e2e)', () => {
   let app: INestApplication;
+  let userRepository: Repository<User>;
   let token: string;
 
   beforeAll(async () => {
@@ -24,6 +27,7 @@ describe('UserModule (e2e)', () => {
     }).compile();
 
     app = module.createNestApplication();
+    userRepository = module.get<Repository<User>>(getRepositoryToken(User));
     await app.init();
   });
 
@@ -144,8 +148,99 @@ describe('UserModule (e2e)', () => {
       });
   });
 
+  describe('userProfile', () => {
+    let userId: number;
+
+    beforeAll(async () => {
+      const [user] = await userRepository.find();
+      userId = user.id;
+    });
+
+    it('should find a user', async () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `query{
+            userProfile(id:${userId}){
+              ok
+              error
+              profile {
+                id
+              }
+            }
+          }`,
+        })
+        .set('X-JWT', token)
+        .expect(200)
+        .expect(res => {
+          const {
+            body: {
+              data: { userProfile },
+            },
+          } = res;
+
+          expect(userProfile.ok).toBeTruthy();
+          expect(userProfile.error).toBeNull();
+          expect(userProfile.profile.id).toBe(userId);
+        });
+    });
+
+    it('should not find a profile', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `query{
+            userProfile(id:2){
+              ok
+              error
+              profile {
+                id
+              }
+            }
+          }`,
+        })
+        .set('X-JWT', token)
+        .expect(200)
+        .expect(res => {
+          const {
+            body: {
+              data: { userProfile },
+            },
+          } = res;
+
+          expect(userProfile.ok).toBeFalsy();
+          expect(userProfile.error).toBe('user not found');
+          expect(userProfile.profile).toBeNull();
+        });
+    });
+
+    it('should not return value when sending wrong jwt', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `query{
+            userProfile(id:${userId}){
+              ok
+              error
+              profile {
+                id
+              }
+            }
+          }`,
+        })
+        .set('X-JWT', 'wrongToken')
+        .expect(200)
+        .expect(res => {
+          const {
+            body: { data },
+          } = res;
+
+          expect(data).toBeNull();
+        });
+    });
+  });
+
   it.todo('me');
-  it.todo('userProfile');
   it.todo('editProfile');
   it.todo('verifyEmail');
 });
